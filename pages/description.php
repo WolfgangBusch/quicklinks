09559078ed72690f0f58023811af7e983150a9ef
Wolfgang Busch <wolfgang@busch-dettum.de>
2020-03-22 19:04:16 +0100

    Add files via upload

--- pages/description.php
+++ pages/description.php
@@ -3,21 +3,21 @@
  * Quicklinks AddOn
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version Juli 2018
+ * @version MÃ¤rz 2020
  */
 #
 # --- Beschreibung
 $string='
 <div><b>Erzeugung der Quicklinks:</b></div>
 <div style="padding-left:20px;">
-Das AddOn stellt zwei Module zur Verfügung, mit denen jeweils eine Gruppe
+Das AddOn stellt zwei Module zur VerfÃ¼gung, mit denen jeweils eine Gruppe
 von Linkzeilen (&quot;Quicklinks-Gruppe&quot;) erzeugt werden kann. Der
 eine Modul definiert eine Gruppe von Links auf Redaxo-Artikel (interne
 Links), der andere eine Liste externer Links.<br/>
 Eine Quicklinks-Gruppe wird dann durch einen Artikel gebildet, der nur
-genau einen der entsprechenden Blöcke enthält. Der Artikel kann
+genau einen der entsprechenden BlÃ¶cke enthÃ¤lt. Der Artikel kann
 <code>offline</code> bleiben, da er keine Frontend-Ausgaben macht.<br/>
-Sinnvollerweise richtet man für für die Quicklinks-Gruppen eine eigene
+Sinnvollerweise richtet man fÃ¼r fÃ¼r die Quicklinks-Gruppen eine eigene
 Kategorie ein. Die Reihenfolge der Gruppen (von links nach rechts)
 ergibt sich aus der Reihenfolge der Artikel in der Kategorie.</div>
 
@@ -30,10 +30,10 @@ Aufruf erfolgt sinnvollerweise im Seitentemplate.</div>
 <div><br/><b>Konfigurierbare Styles:</b></div>
 <ul>
     <li>Breite der Gruppen und Linkzeilen</li>
-    <li>Textgröße der Gruppen (die der Linkzeilen ist 20% kleiner)</li>
+    <li>TextgrÃ¶ÃŸe der Gruppen (die der Linkzeilen ist 20% kleiner)</li>
     <li>Hintergrundfarbe, Textfarbe und Randfarbe der Gruppen</li>
     <li>Hintergrundfarbe und Textfarbe der Linkzeilen</li>
 </ul>
 ';
-echo utf8_encode($string);
+echo $string;
 ?>
--- pages/example.php
+++ pages/example.php
@@ -3,7 +3,7 @@
  * Quicklinks AddOn
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version Juli 2018
+ * @version März 2020
  */
 echo quicklinks::xmp_linklists();
 ?>
