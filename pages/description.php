915d670a2bdfdd09f9ef56eaf41a374fcafbc71e
Wolfgang Busch <wolfgang@busch-dettum.de>
2024-12-28 11:50:43 +0100

    Add files via upload

--- pages/description.php
+++ pages/description.php
@@ -3,7 +3,7 @@
  * Quicklinks AddOn
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version Januar 2022
+ * @version Dezember 2024
  */
 #
 # --- Beschreibung
@@ -12,26 +12,28 @@ echo '
 
 <div>Das AddOn stellt zwei Module zur Erzeugung jeweils einer
 Gruppe von Linkzeilen ("Quicklinks-Gruppe") zur Verfügung.
-Der eine Modul definiert eine Liste von internen Links (auf
-Redaxo-Artikel), der andere eine Gruppe externer Links.<br/>
-An passender Stelle wird ein Artikel angelegt. Dessen Blöcke
-aus den oben genannten Modulen bilden die Quicklinks-Gruppen.
-Die Reihenfolge der Blöcke im Artikel bestimmt die Reihenfolge
-der Darstellung der Quicklinks-Gruppen. Der Artikel kann
-<i>offline</i> bleiben, da er keine Frontend-Ausgaben macht.</div>
+Der eine Modul definiert eine Liste von internen Links, d.h.
+Links auf Redaxo-Artikel, der andere eine Gruppe externer
+Links. An passender Stelle wird ein (in der Regel nur ein
+einziger) Artikel angelegt, dessen Blöcke aus den oben
+genannten Modulen erzeugt werden. Jeder dieser Blöcke
+definiert eine Quicklinks-Gruppe. Die Reihenfolge der Blöcke
+im Artikel bestimmt die Reihenfolge der Darstellung der
+Quicklinks-Gruppen. Der Artikel kann <i>offline</i> bleiben,
+da er keine Frontend-Ausgaben macht.</div>
 
-<div><br/><b>Anzeige der Quicklinks:</b></div>
+<div><br><b>Anzeige der Quicklinks:</b></div>
 
 <div>Die Quicklinks-Gruppen werden durch den Aufruf der
 PHP-Funktion <code>quicklinks::print_quicklinks()</code>
 angezeigt. Dieser Aufruf erfolgt sinnvollerweise im
 Seitentemplate.</div>
 
-<div><br/><b>Konfigurierbare Styles:</b></div>
+<div><br><b>Konfigurierbare Styles:</b></div>
 
-<div>- Breite der Gruppen und Linkzeilen<br/>
-- Textgröße der Gruppen (die der Linkzeilen ist 20% kleiner)<br/>
-- Hintergrundfarbe, Textfarbe und Randfarbe der Gruppen<br/>
-- Hintergrundfarbe und Textfarbe der Linkzeilen</div><br/>
+<div>- Breite der Gruppen und Linkzeilen<br>
+- Textgröße der Gruppen (die der Linkzeilen ist 20% kleiner)<br>
+- Hintergrundfarbe, Textfarbe und Randfarbe der Gruppen<br>
+- Hintergrundfarbe und Textfarbe der Linkzeilen</div><br>
 ';
 ?>
--- pages/settings.php
+++ pages/settings.php
@@ -3,7 +3,7 @@
  * Quicklinks AddOn
  * @author wolfgang[at]busch-dettum[dot]de Wolfgang Busch
  * @package redaxo5
- * @version Januar 2022
+ * @version Dezember 2024
  */
 quicklinks::print_form();
 ?>
